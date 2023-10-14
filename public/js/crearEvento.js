c0ab59e0744f36cbe5e9dd7700fa7a0360aa0a3f
Jorge2610 <jorge261093@gmail.com>
2023-10-13 22:32:26 -0400

    Crear evento form terminado.

--- public/js/crearEvento.js
+++ public/js/crearEvento.js
@@ -3,7 +3,8 @@ let fechaInscripcionFin = document.getElementById('fechaInscripcionFin')
 let inputGenero = document.getElementById('generoCheck');
 let inputEdad = document.getElementById('edadCheck');
 let inputCosto = document.getElementById('eventoPagoCheck');
-let form =document.getElementById("formularioCrearEvento")
+let form = document.getElementById("formularioCrearEvento")
+let edadCheck = document.getElementById("edadCheck");
 
 fechaInscripcionInicio.addEventListener('change', (e) => {
   let valor = e.target.value
@@ -18,10 +19,19 @@ fechaInicio.addEventListener('change', (e) => {
   fechaFin.setAttribute('min', valor)
 })
 
+let edadMinima = document.getElementById("edadMinima");
+let edadMaxima = document.getElementById("edadMaxima");
+
+edadMinima.addEventListener('change', (e) => {
+  let valor = e.target.value;
+  edadMaxima.value = valor;
+  edadMaxima.min = valor;
+})
+
 function previewAfiche(event) {
   var reader = new FileReader();
   reader.readAsDataURL(event.target.files[0]);
-  reader.onloadend = function() {
+  reader.onloadend = function () {
     var img = document.getElementById('afiche');
     img.setAttribute('src', reader.result)
   };
@@ -29,7 +39,7 @@ function previewAfiche(event) {
 
 function previewSponsorLogo(event) {
   var reader = new FileReader();
-  reader.onload = function() {
+  reader.onload = function () {
     var output = document.getElementById('sponsorPreview');
     output.style.backgroundImage = 'url(' + reader.result + ')';
   };
@@ -46,47 +56,41 @@ function resetModal(idModal, idForm) {
   form.classList.remove('was-validated');
 }
 
-function prueba(){
-  let genero = document.getElementById('genero');
-  console.log(genero.value);
-}
-
-function prueba1(){
-  let genero = document.getElementById('limiteDeEdad');
-  console.log(typeof(genero.value));
-}
-
-function utilizarInput(indInput,check){
+function utilizarInput(indInput, check) {
   let input = document.getElementById(indInput);
   input.disabled = !check;
 }
 
-function mostrarInput(indInput,check){
+function mostrarInput(indInput, check) {
   let input = document.getElementById(indInput);
-  if(!check){
-    input.style.display= "none";
+  if (!check) {
+    input.style.display = "none";
   }
-  else{
+  else {
     input.style.display = "flex";
   }
 }
 
 //check
-inputGenero.addEventListener("change",()=>{
-  mostrarInput('genero',inputGenero.checked);
+inputGenero.addEventListener("change", () => {
+  mostrarInput('genero', inputGenero.checked);
 });
-inputEdad.addEventListener("change",()=>{
-  mostrarInput('limiteDeEdad',inputEdad.checked);
+inputEdad.addEventListener("change", () => {
+  mostrarInput('limiteDeEdad', inputEdad.checked);
 });
-inputCosto.addEventListener('change',()=>{
-  mostrarInput('eventoPago',inputCosto.checked);
+inputCosto.addEventListener('change', () => {
+  mostrarInput('eventoPago', inputCosto.checked);
 });
 //validacion
-form.addEventListener("submit",(event)=>{
+form.addEventListener("submit", (event) => {
   if (!form.checkValidity()) {
     event.preventDefault()
     event.stopPropagation()
   }
 
   form.classList.add('was-validated')
+});
+
+edadCheck.addEventListener('change', () => {
+  mostrarInput('rangosDeEdad', edadCheck.checked);
 });
