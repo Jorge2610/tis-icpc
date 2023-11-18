bfd7ea84b9342ab510f3f3957af0f67d61b1665b
nnpqo <yaninapaolaquispeorellana@gmail.com>
2023-11-17 22:21:17 -0400

    cambio en el sider

--- resources/views/components/sider/tipo-actividad.blade.php
+++ resources/views/components/sider/tipo-actividad.blade.php
@@ -1,11 +1,11 @@
 <div class="accordion-item border-0">
-    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+    <h2 class="accordion-header" id="panelsStayOpen-headingSix">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-            data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+            data-bs-target="#panelsStayOpen-collapseSix" aria-expanded="true" aria-controls="panelsStayOpen-collapseSix">
             TIPO DE ACTIVIDAD
         </button>
     </h2>
-    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+    <div id="panelsStayOpen-collapseSix" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingSix">
         <div class="my-1 ms-3">
             <a href="{{ url('/admin/tipos-de-actividad/crear-tipo') }}"
                 class="list-group-item list-group-item-action py-2 border-0"
