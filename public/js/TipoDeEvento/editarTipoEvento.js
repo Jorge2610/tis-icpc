b66d86ccc8f1f076aeea52a81fc4ba819d6c45df
Rickybk <doriflolo@gmail.com>
2023-11-26 18:57:27 -0400

    "Fix editar tipo de evento"

--- public/js/TipoDeEvento/editarTipoEvento.js
+++ public/js/TipoDeEvento/editarTipoEvento.js
@@ -1,7 +1,12 @@
 let tablaDeTipos;
 let tablaInicializada = false;
-let formulario = document.getElementById("formularioTipoEvento");
-
+const form = document.getElementById("formularioTipoEvento");
+const idTipoEvento = document.getElementById("id");
+const botonCancelar = document.getElementById("cancelarBoton");
+const inputNombre = document.getElementById("nombreTipoEvento");
+const mensajeNombre = document.getElementById("mensajeNombre");
+const inputDescripcion = document.getElementById("detalleTipoEvento");
+let nombreAnterior = "";
 const dataTableOptions = {
     pageLength: 10,
     lengthMenu: [5, 10, 15, 20],
@@ -24,61 +29,106 @@ const dataTableOptions = {
     },
 };
 
-const remover = () => {
-    formulario.classList.remove("was-validated");
-    formulario.reset();
-};
-
-const initDataTable = async () => {
-    if (tablaInicializada) {
-        tablaDeTipos.destroy();
-    }
-    DataTable.datetime("DD-MM-YYYY");
-    tablaDeTipos = $("#tablaTipoDeEvento").DataTable(dataTableOptions);
-    tablaInicializada = true;
-};
-
-//Editar evento
-function editarTipoEvento(id) {
-    // Obtener el formulario
-    const formularioTipoEvento = document.getElementById('formularioTipoEvento');
-    console.log(id);
-    console.log(formularioTipoEvento);
-    // Agregar un manejador de eventos al formulario
-    formularioTipoEvento.addEventListener('submit', (event) => {
-        event.preventDefault();
-        formularioTipoEvento.classList.add("was-validated");
-        const formData = new FormData(formularioTipoEvento);
-        axios.post(`/api/tipo-evento/actualizar/${id}`, formData)
-            .then((response) => {
-                if (response.data.error === false) {
-                    // El tipo de evento se actualizó con éxito, puedes redirigir o mostrar un mensaje de éxito.
-                    mostrarAlerta(
-                        "Éxito",
-                        response.data.mensaje,
-                        response.data.error ? "danger" : "success"
-                    );
-                    recargarEventos(); // Reemplaza esto con la URL de redirección deseada
-                } else {
-                    // Manejar errores, por ejemplo, mostrar un mensaje de error.
-                    //alert(response.data.mensaje);
+const editarTipoEvento = (form) => {
+    //Si el tipo de evento existe entonces se guarda este valor y servirá para la validación
+    nombreAnterior = inputNombre.value
+    console.log("Este es el id que obtenemos del formulario " + idTipoEvento.value);
+    axios.post(`/api/tipo-evento/actualizar/${idTipoEvento.value}`, form)
+    .then(function(response){
+        const mensaje = response.data.mensaje
+        const nombreIgual = 'El tipo de evento ya existe'
+        mostrarAlerta(
+            "Éxito",
+            mensaje,
+            response.data.error ? "danger" : "success"
+        );
+        /**Si existe un tipo de evento con el mismo nombre, se mantiene los datos del formulario**/
+        if(mensaje === nombreIgual){
+            inputNombre.classList.remove("is-valid")
+            inputNombre.classList.add("is-invalid")    
+            inputDescripcion.classList.add("is-valid")
+            mensajeNombre.textContent = 'El tipo de evento ya existe'
+            console.log("Este es el inputNombre "+inputNombre);
+        }else{
+            form.querySelectorAll(".form-control, .form-select").forEach(
+                (Element) => {
+                    Element.classList.remove("is-valid");
                 }
-            })
-            .catch((error) => {
-                // Manejar errores de la solicitud Axios
-                console.error(error);
-            });
-    });
-}
-
-const recargarEventos = () => {
-    setTimeout(() => {
-        window.location.href = "/admin/tipos-de-evento/editar-tipo";
-    }, 1750);
+            );
+            //form.reset();
+        }   
+    }).catch(function(error){
+        console.log(mensaje);
+        mostrarAlerta("Error", "Hubo un error al guardar el tipo de evento", "danger");
+    })
 };
 
-window.addEventListener("load", async () => {
-    await initDataTable();
+form.addEventListener("submit",(event) =>{
+    event.preventDefault()
+    form.querySelectorAll(".form-control, .form-select").forEach((Element) => {
+        Element.dispatchEvent(new Event("change"));
+    });
+    if(validar()){
+        const formData = new FormData(form);
+        editarTipoEvento(formData);
+    }
+})
+
+const validar = () => {
+    return form.querySelector(".is-invalid") === null;
+};
+
+//Agregar validación a los inputs
+form.querySelectorAll(".form-control, .form-select").forEach((Element) => {
+    Element.addEventListener("change", () => {
+        if (Element.hasAttribute("required") && Element.value === "") {
+            Element.classList.remove("is-valid");
+            Element.classList.add("is-invalid");
+        } else {
+            Element.classList.remove("is-invalid");
+            Element.classList.add("is-valid");
+        }
+    });
 });
 
+function validarNombreRepetido(){
+    //Validaciones antes de que se guarde el nombre repetido
+    if(nombreAnterior === ''){
+        if(inputNombre.value === ''){
+            inputNombre.classList.remove("is-valid");
+            inputNombre.classList.add("is-invalid");
+            mensajeNombre.textContent = "El nombre no puede estar vacío.";
+        }else{
+            inputNombre.classList.remove("is-invalid");
+            inputNombre.classList.add("is-valid");
+        }
+    }else{
+        if(inputNombre.value !== nombreAnterior && inputNombre.value !== '') {
+            inputNombre.classList.remove("is-invalid");
+            inputNombre.classList.add("is-valid");
+            mensajeNombre.textContent = "";
+        }else {
+            if (inputNombre.value === "") {
+                inputNombre.classList.remove("is-valid");
+                inputNombre.classList.add("is-invalid");
+                mensajeNombre.textContent = "El nombre no puede estar vacío.";
+            } else {
+                inputNombre.classList.remove("is-valid");
+                inputNombre.classList.add("is-invalid");
+                mensajeNombre.textContent = "El tipo de evento ya existe";
+            }
+        }
+    }
+}
 
+inputNombre.addEventListener("input",validarNombreRepetido );
+inputNombre.addEventListener("change",validarNombreRepetido );
+
+function quitarValidacion(){
+    form.querySelectorAll(".form-control, .form-select").forEach(
+        (Element) => {
+            Element.classList.remove("is-valid");
+            Element.classList.remove("is-invalid");
+        }
+    );
+}
