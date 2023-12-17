c20fdfbc66bc594d7c52b7655a1a7ddf80bdc6c7
Ivan Palacios <ivpalacios47@gmail.com>
2023-12-17 19:58:53 -0400

    Fix mensaje de confirmacion

--- resources/views/emails/equipo/confirmacion.blade.php
+++ resources/views/emails/equipo/confirmacion.blade.php
@@ -1,7 +1,12 @@
 @component('mail::message')
 Hola,
 
-Parece que usted quiere inscribir a su equipo <strong> {{ $equipo->nombre }} </strong> en el evento <strong> {{ $evento->nombre }} </strong>, confirme su participación pulsando el siguiente botón:
+Parece que usted quiere inscribir a su equipo <strong> {{ $equipo->nombre }} </strong> en el evento <strong> {{ $evento->nombre }} </strong>,copie el siguiente código de
+acceso:
+
+<strong> {{ $equipo->codigo }} </strong>
+
+o presione el siguiente botón:
 
 @component('mail::button', ['url' => url('confirmar/equipo/' . $equipo->codigo)])
 Confirmar participación
