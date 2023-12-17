1d5fe5c1dfc96e5d03c136d9d78258e95373deae
Ivan Palacios <ivpalacios47@gmail.com>
2023-12-17 00:56:01 -0400

    Add ruta verificar codigo participante api

--- routes/api.php
+++ routes/api.php
@@ -119,6 +119,7 @@ Route::group(
         Route::get('{id}', 'participantesEvento');
         Route::post('/', 'inscribirEvento');
         Route::post('/enviarCodigo/{id}/{id2}', 'enviarCodigoCorreo');
+        Route::post('/verificarCodigo/{id}', 'verificarCodigo');
         Route::get('/participantes', 'participantesEvento');
         Route::any('/verificar/{id}', 'verificarParticipante');
     }
